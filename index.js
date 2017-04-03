94a160d57c8b1c08be06e6c9bd1737181d0f9200
Dominik Mast <dominik-mast@web.de>
2017-04-03 04:57:38 +0000

    complete

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require("express");
 var app = express();
 
+
+
 app.get("/", function(req, res){
+    var ip = req.headers["x-forwarded-for"];
+    var sprache = req.acceptsLanguages();
+    var software = req.headers["user-agent"];
     
-    
-    res.send("hello world");
+   res.json({
+       
+    ip: ip,
+    language: sprache,
+    software: software
+       
+   });
+   
 });
+
+
 
 app.listen((process.env.PORT||8080|| 5000), function(){
     console.log("roger, we are online...");
